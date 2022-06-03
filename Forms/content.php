56fe98e9396374648ae5c7e43efcfb94af1812e0
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2022-06-03 20:01:30 +0200

    forms

---
+++ Forms/content.php
@@ -0,0 +1,7 @@
+<?php
+    if(isset($_POST["userName"])){
+        echo "Hello " . $_POST["userName"];
+        die();
+    }
+    
+?>
---
+++ Forms/form.php
@@ -0,0 +1,4 @@
+<form action="content.php" method="POST">
+    <input type="text" name="userName">
+    <input type="submit">
+</form>
